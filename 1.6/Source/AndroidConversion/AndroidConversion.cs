5e854cd0140ecf501595cc2ee1479c06f7b8f7a7
unknown <62079853+Derp88@users.noreply.github.com>
2025-07-18 04:26:43 -0400

    Working version

---
+++ 1.6/Source/AndroidConversion/AndroidConversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace AndroidConversion
+{
+    public class IngestionOutcomeDoer_ChangeXenotype : IngestionOutcomeDoer
+    {
+        public XenotypeDef destinationXenotype;
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            foreach (Gene gene in pawn.genes?.GenesListForReading)
+            {
+                pawn.genes?.RemoveGene(gene);
+            }
+            pawn.genes?.SetXenotype(destinationXenotype);
+        }
+    }
+}
